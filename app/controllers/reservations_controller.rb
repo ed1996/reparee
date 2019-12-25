2b6166ff07e3d81d5584488e09baf6d6f79a11a7
Edgar Bekier <edgar.bekier@devinci.fr>
2019-12-25 16:17:26 +0100

    Creation controller reservation

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,15 @@
+class ReservationsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @reservation = current_user.reservations.create(reservation_params)
+    redirect_to @reservation.entreprise, notice: "Votre réservation a été acceptée"
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :hour, :minute, :description, :address, :phone, :postal_code, :entreprise_id)
+  end
+
+end
