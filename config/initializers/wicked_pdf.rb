c11848a7361e099e97b5f9be17dc40b13bd9f258
Edgar Bekier <sieegate@gmail.com>
2021-05-11 23:20:32 +0200

    test pour affichage pdf heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -11,8 +11,8 @@
 WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
-   exe_path: '/usr/local/bin/wkhtmltopdf',
-  enable_local_file_access: true
+ #  exe_path: '/usr/local/bin/wkhtmltopdf',
+ # enable_local_file_access: true
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
