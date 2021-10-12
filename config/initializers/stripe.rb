a0587a163fc90c3a6dd38498faccf2195e81b8bd
Edgar Bekier <sieegate@gmail.com>
2021-10-12 22:12:25 +0200

    api stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_vZZJYxpcq1AYeYg6NCDt7Oz6',
-    :secret_key      => 'sk_test_u3S6j4XYQDBJqImSaNug0Jbu'
+    publishable_key: Rails.application.secrets.stripe[:publishable_key],
+    secret_key: Rails.application.secrets.stripe[:secret_key]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
