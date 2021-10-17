a146278fc53c8945134c561a99a3aae2c5389da4
Edgar Bekier <sieegate@gmail.com>
2021-10-17 19:27:48 +0200

    correction delete user in admin panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  has_many :entreprises
-  has_many :reservations
-  has_many :reviews
-  has_many :factures
+  has_many :entreprises, dependent: :delete_all
+  has_many :reservations, dependent: :delete_all
+  has_many :reviews, dependent: :delete_all
+  has_many :factures, dependent: :delete_all
 end
